9634b4d8edc9ca6f95598095a6723b32ced02d2a
Pablo Renero Balgañón <prenero4bc@gmail.com>
2020-12-05 10:59:26 +0100

    Added day2 sol

--- day2/sol.py
+++ day2/sol.py
@@ -1,9 +1,11 @@
-from itertools import combinations
 import sys
 
 if __name__ == '__main__':
-    data = sys.stdin.read().split('\n')[0:-1]
-    data = list(map(int, data))
-    data = list(combinations(data, 3))
-    result = next(filter(lambda x : x[0] + x[1] + x[2] == 2020, data ))
-    print(result[0] * result[1] * result[2])
+    sum = 0
+    for line in sys.stdin.readlines():
+        min, max = line.replace('\n', '').replace(':', '').split('-')
+        max, letter, passwd = max.split(' ')
+        occurences = passwd.count(letter)
+        if int(min) <= occurences and occurences <= int(max):
+            sum += 1
+    print(sum)
