458ab2312edeb05c8aaa2bbe0aa3458e6a298053
Pablo Renero Balgañón <prenero4bc@gmail.com>
2020-12-06 09:45:41 +0100

    Day 5 part 2

---
+++ day5/sol2.ts
@@ -0,0 +1,69 @@
+import * as fs from "fs";
+import {exit} from "process";
+// Functions
+function fileRead(fileRoute: string): string {
+  return fs.readFileSync(fileRoute, "utf8");
+}
+
+function* fileByLines(fileRoute: string) {
+  const data: string[] = fileRead(fileRoute)
+    .split("\n")
+    .map((a) => a.replace("\r", ""));
+  data.pop();
+  for (let i of data) {
+    yield i;
+  }
+  return "done";
+}
+
+function readAll(fileRoute: string): string[] {
+  const data: string[] = fileRead(fileRoute)
+    .split("\n")
+    .map((a) => a.replace("\r", ""));
+  data.pop();
+  return data;
+}
+
+function findNumber(
+  data: string,
+  start: number,
+  end: number,
+  lower: String,
+  upper: String
+): number {
+  let L: number = start;
+  let R: number = end;
+  let index: number = -1;
+  for (let letter of data) {
+    index = L + Math.ceil((R - L) / 2);
+    /* console.log(`index: ${index} L: ${L} R: ${R} value: ${letter}`); */
+    if (letter === lower) {
+      R = index - 1;
+    } else if (letter === upper) {
+      L = index;
+    }
+  }
+  return data[data.length - 1] === lower ? index - 1 : index;
+}
+
+const FILE_DIR: string = "./";
+const FILE_NAME: string = "in";
+let data: string[] = readAll(FILE_DIR + FILE_NAME);
+let sitList = data
+  .map((line) => {
+    let row: string;
+    let col: string;
+    let match = line.match(/^([FB]{7})([LR]{3})$/);
+    row = match[1];
+    col = match[2];
+    let block: number = findNumber(row, 0, 127, "F", "B");
+    let sitNum: number = findNumber(col, 0, 7, "L", "R");
+    return block * 8 + sitNum;
+  })
+  .sort();
+for (let i = 1; i < sitList.length; i++){
+  if ( sitList[i] - sitList[i-1] !== 1){
+    console.log(sitList[i] - 1);
+    exit();
+  }
+}
